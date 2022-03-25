139158ec400038ab2c856e496701e185bd3259f8
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-03-25 14:14:02 +0100

    changed search and added unknow search

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import data from "./data.js";
 import { Recipe } from "./recipe.js";
 
-//new branch, change the filter/find in search.. & prepare, do the algorigramme
-
 class Main {
   constructor() {
     this.ingredientArray = [];
@@ -17,7 +15,7 @@ class Main {
     });
     this.displayRecipes(this.recipes);
     this.searchTag = { ingredient: "", appliance: "", ustensil: "" };
-    this.extractTagsFromRecipes();
+    this.extractTagsFromRecipes(this.recipes);
     this.displayTagsInDropdown();
     this.mainSearchBar();
     const ingredientInput = document.querySelector(".ingredient-input");
@@ -39,9 +37,14 @@ class Main {
     this.searchBar.addEventListener("keyup", (e) => {
       e.preventDefault();
       console.log(this.recipes);
-      this.searchString = e.target.value.toLowerCase();
+      if (e.target.value.length < this.searchString.length) {
+        this.searchString = e.target.value.toLowerCase();
+        this.filtersRecipe();
+      } else {
+        this.searchString = e.target.value.toLowerCase();
 
-      this.filtersRecipe();
+        if (this.searchString.length >= 3) this.filtersRecipe();
+      }
     });
   }
 
@@ -52,6 +55,10 @@ class Main {
       const recipeCard = recipe.generateDomCard();
       recipesContainer.appendChild(recipeCard);
     });
+    if (recipes.length === 0) {
+      recipesContainer.innerHTML =
+        "Aucune recette ne correspond à votre demande";
+    }
   }
 
   addAndDeleteFilters(filterTag) {
@@ -87,7 +94,6 @@ class Main {
   }
 
   filtersRecipe() {
-    // à modifier filter pour seconde branche
     const matchingRecipes = this.recipes.filter((recipe) => {
       return recipe.isMatchingAllFilters(
         this.ingredientArray,
@@ -101,9 +107,11 @@ class Main {
     console.log(this.ustensilesArray);
     console.log(matchingRecipes);
     this.displayRecipes(matchingRecipes);
+    this.extractTagsFromRecipes(matchingRecipes);
+    this.displayTagsInDropdown();
   }
 
-  //open the tagList by clicking on the chevrons
+  //open the dropdown by clicking on the chevrons
   chevronClick(e) {
     const list = e.currentTarget.nextElementSibling;
     const chevron = e.currentTarget.children[0];
@@ -124,8 +132,8 @@ class Main {
     list.classList.toggle("open");
   }
 
-  extractTagsFromRecipes() {
-    this.recipesElements = this.recipes.reduce(
+  extractTagsFromRecipes(filteredRecipes) {
+    this.recipesElements = filteredRecipes.reduce(
       (total, cur) => {
         cur.ingredients.forEach((ing) => {
           if (!total.ingredients.includes(ing.ingredient)) {
